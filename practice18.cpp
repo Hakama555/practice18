b3367216172c29ddc13a525ae3c5c58051d3e792
Hakama555 <54787616+Hakama555@users.noreply.github.com>
2020-08-17 11:10:22 +0900

    プロジェクト ファイルを追加します。

---
+++ practice18.cpp
@@ -0,0 +1,19 @@
+﻿// practice18.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+int main(void)
+{
+    int a = 1;
+
+    while (a <= 5)
+    {
+        printf("%d回目の繰り返しです\n" , a);
+        a++;
+
+        }
+
+    return 0;
+    
+   }
